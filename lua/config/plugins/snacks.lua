0cb3a449c4d1d4623289e10fa1df5ac9747f4e2e
Andrew Richard <acrichard07@gmail.com>
2025-04-04 09:39:35 -0700

    reenabled snacks
    i reenabled some of the snacks options

--- lua/config/plugins/snacks.lua
+++ lua/config/plugins/snacks.lua
@@ -1,5 +1,5 @@
 return {
-  enabled = false,
+  enabled = true,
   "folke/snacks.nvim",
   priority = 1000,
   lazy = false,
@@ -107,42 +107,42 @@ return {
     -- { "<leader>su", function() Snacks.picker.undo() end, desc = "Undo History" },
     -- { "<leader>uC", function() Snacks.picker.colorschemes() end, desc = "Colorschemes" },
     -- LSP
-    {
-      "gd",
-      function()
-        Snacks.picker.lsp_definitions()
-      end,
-      desc = "Goto Definition",
-    },
-    {
-      "gD",
-      function()
-        Snacks.picker.lsp_declarations()
-      end,
-      desc = "Goto Declaration",
-    },
-    {
-      "gr",
-      function()
-        Snacks.picker.lsp_references()
-      end,
-      nowait = true,
-      desc = "References",
-    },
-    {
-      "gI",
-      function()
-        Snacks.picker.lsp_implementations()
-      end,
-      desc = "Goto Implementation",
-    },
-    {
-      "gy",
-      function()
-        Snacks.picker.lsp_type_definitions()
-      end,
-      desc = "Goto T[y]pe Definition",
-    },
+    -- {
+    --   "gd",
+    --   function()
+    --     Snacks.picker.lsp_definitions()
+    --   end,
+    --   desc = "Goto Definition",
+    -- },
+    -- {
+    --   "gD",
+    --   function()
+    --     Snacks.picker.lsp_declarations()
+    --   end,
+    --   desc = "Goto Declaration",
+    -- },
+    -- {
+    --   "gr",
+    --   function()
+    --     Snacks.picker.lsp_references()
+    --   end,
+    --   nowait = true,
+    --   desc = "References",
+    -- },
+    -- {
+    --   "gI",
+    --   function()
+    --     Snacks.picker.lsp_implementations()
+    --   end,
+    --   desc = "Goto Implementation",
+    -- },
+    -- {
+    --   "gy",
+    --   function()
+    --     Snacks.picker.lsp_type_definitions()
+    --   end,
+    --   desc = "Goto T[y]pe Definition",
+    -- },
     -- {
     --   '<leader>ss',
     --   function()
@@ -215,13 +215,13 @@ return {
     --   desc = 'Git Browse',
     --   mode = { 'n', 'v' },
     -- },
-    {
-      "<leader>gg",
-      function()
-        Snacks.lazygit()
-      end,
-      desc = "Lazygit",
-    },
+    -- {
+    --   "<leader>gg",
+    --   function()
+    --     Snacks.lazygit()
+    --   end,
+    --   desc = "Lazygit",
+    -- },
     {
       "<leader>un",
       function()
